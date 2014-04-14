9c39189e4f7c76d7d0657c04cb4bacbde6c5f205
Tim Skalland <skallandt@gmail.com>
2014-04-13 23:12:16 -0700

    Tim added a bit of code

--- Project1.R
+++ Project1.R
@@ -26,3 +26,35 @@
 
 
 # Tim's Code Below
+library(dplyr)
+??dplyr
+setwd("C:/Documents and Settings/Tim Skalland/Desktop/ST 599 - Big Data/csv_hca")
+hca2012 <- read.csv("ss12hca.csv", header=T, nrows=1000, stringsAsFactors = FALSE)
+hca2012_df <- tbl_df(hca2012)
+hca2012_df <- select(hca2012_df, TEN, HINCP)
+hca2012_df
+
+## Note for TEN variable: 
+#1 .Owned with mortgage or loan (include home equity loans)
+#2 .Owned free and clear
+#3 .Rented
+#4 .Occupied without payment of rent
+
+hca2012_rm.na <- filter(hca2012_df, TEN != "NA") 
+acs_tenure <- group_by(hca2012_rm.na, TEN)
+summarize(acs_tenure, 
+          avg_inc = mean(HINCP, na.rm = TRUE))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
