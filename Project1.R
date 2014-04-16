7ed034a27c64b9af9fd90641a4c7ef6694425780
pahukulj <pahukulj@onid.orst.edu>
2014-04-16 09:48:23 -0700

    added abbreviations

--- Project1.R
+++ Project1.R
@@ -90,6 +90,66 @@ summarize(income_ten, med_income = median(HINCP, na.rm= TRUE))
 
 # what kind of summaries do we need to find
 
+### abreviations 
+# Coding the ADJHSG variable to specifications above
+ADJHSG_codes <- c("1053092" = "2010",
+                  "1020890" = "2011",
+                  "1000000" = "2012")
+ca_3yr_df <- mutate(ca_3yr_df, Year = ADJHSG_codes[as.character(ADJHSG)])
+
+# Coding the ST variable to specifications above
+ST_codes <- c("1" = "AL",
+              "2" = "AK",
+              "4" = "AZ",
+              "5" = "AR",
+              "6" = "CA",
+              "8" = "CO",
+              "9" = "CT",
+              "11" = "DC" ,
+              "12" = "FL" ,
+              "13" = "GA" ,
+              "15" = "HI" ,
+              "16" = "ID" ,
+              "17" = "IL" ,
+              "18" = "IN" ,
+              "19" = "IA" ,
+              "20" = "KS" ,
+              "21" = "KY" ,
+              "22" = "LA" ,
+              "23" = "ME" ,
+              "24" = "MD" ,
+              "25" = "MA" ,
+              "26" = "MI" ,
+              "27" = "MN" ,
+              "28" = "MS" ,
+              "29" = "MO" ,
+              "30" = "MT" ,
+              "31" = "NE" ,
+              "32" = "NV" ,
+              "33" = "NH" ,
+              "34" = "NJ" ,
+              "35" = "NM" ,
+              "36" = "NY" ,
+              "37" = "NC" ,
+              "38" = "ND" ,
+              "39" = "OH" ,
+              "40" = "OK" ,
+              "41" = "OR" ,
+              "42" = "PA" ,
+              "44" = "RI" ,
+              "45" = "SC" ,
+              "46" = "SD" ,
+              "47" = "TN" ,
+              "48" = "TX" ,
+              "49" = "UT" ,
+              "50" = "VT" ,
+              "51" = "VA" ,
+              "53" = "WA" ,
+              "54" = "WV" ,
+              "55" = "WI" ,
+              "56" = "WY" , 
+              "72" = "PR" ,)
+ca_3yr_df <- mutate(ca_3yr_df, States = ST_codes[as.character(ST)])
 
 
 
